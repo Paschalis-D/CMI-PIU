b7eebf7c2d75e7226e2a8204d3712aff1bd39090
or4ngepoop <doboudispasxalis@gmail.com>
2024-11-01 22:54:10 +0200

    Initial plan for tabular info class

---
+++ data_prep/tabular_info.py
@@ -0,0 +1,16 @@
+
+
+class get_info():
+    def __init__(self):
+        pass
+
+    def get_missing_values(self):
+        pass
+
+    def get_demographics(self):
+        pass
+
+    def get_correlation(self):
+        pass
+
+    
