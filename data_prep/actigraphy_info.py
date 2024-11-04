51f259f37a304733aacbde50b751e8efd6522e87
or4ngepoop <doboudispasxalis@gmail.com>
2024-11-04 22:57:58 +0200

    Got correlation data for actigraphy info

---
+++ data_prep/actigraphy_info.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from matplotlib.ticker import MaxNLocator, FormatStrFormatter, PercentFormatter
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+
+class GetActigraphyInfo:
+    def __init__(self, parquet_path: str):
+        self.df = pd.read_parquet(parquet_path)
+        self.df = self.get_acc_magn(self.df)
+        self.means = self.get_means(self.df)
+
+    def get_acc_magn(self, df: pd.DataFrame):
+        """
+        This function will calculate the magnitude of the acceleration based on the values of the X,Y,Z acceleraton measured by the wrist watch
+        """
+        df['acc_magn'] = np.sqrt(df['X']**2 + df['Y']**2 + df['Z']**2)
+        return df
+    
+    def get_means(self, df: pd.DataFrame):
+        """
+        This function will creturn the mean values for the acc_magn, enmo, anglez, light, battery_voltage and return them 
+        """
+        acc_magn_mean = df['acc_magn'].mean()
+        enmo_mean = df['enmo'].mean()
+        anglez_mean = df['anglez'].mean()
+        light_mean = df['light'].mean()
+        battery_voltage_mean = df['battery_voltage'].mean()
+        return [acc_magn_mean, enmo_mean, anglez_mean, light_mean, battery_voltage_mean]
+
+
+if __name__ == '__main__':
+    ROOT_DIR = os.getcwd()
+    ACTIGRAPHY_DIR = os.path.join(ROOT_DIR, 'data/series_train.parquet')
+    TRAIN_CSV_PATH = os.path.join(ROOT_DIR, 'data/train.csv')
+    all_means = []
+    for id in os.listdir(ACTIGRAPHY_DIR):
+        parquet_file = os.path.join(ACTIGRAPHY_DIR, f'{id}/part-0.parquet')
+        info = GetActigraphyInfo(parquet_file)
+        id_means = info.means
+        id_string = id.split('=')[1]
+        id_means.insert(0, id_string)
+        all_means.append(id_means)
+    column_names = ["id", "acc_magn_mean", "enmo_mean", "anglez_mean", "light_mean", "battery_voltage_mean"]
+    all_means_df = pd.DataFrame(all_means, columns=column_names)
+    train_csv_df = pd.read_csv(TRAIN_CSV_PATH)
+    merged_df = all_means_df.merge(train_csv_df[['id', 'sii']], on='id', how='left')
+    
+    numeric_data = merged_df.select_dtypes(include=[np.number])
+    correlation_matrix = numeric_data.corr()
+
+    plt.figure(figsize=(20, 20))
+    sns.heatmap(correlation_matrix, annot=False, fmt=".2f", cmap="coolwarm", vmin=-1, vmax=1)
+    plt.title("Feature Correlation Matrix")
+    plt.savefig(os.path.join("data_info", "correlation_matrix_actigraphy.png"))
+    plt.close()
+
